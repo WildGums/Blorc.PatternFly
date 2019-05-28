49d5aad0ec04f54c784529786557ff0165dc0fb3
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2019-05-28 10:51:33 -0400

    Add Accordion component

---
+++ src/Blazor.PatternFly/Components/Accordion/AccordionComponent.cs
@@ -0,0 +1,25 @@
+﻿namespace Blazor.PatternFly.Components.Accordion
+{
+    using Microsoft.AspNetCore.Components;
+
+    public class AccordionComponent : ComponentBase
+    {
+        private int _selectedIndex;
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        public int SelectedIndex
+        {
+            get => _selectedIndex;
+            set
+            {
+                if (_selectedIndex != value)
+                {
+                    _selectedIndex = value;
+                    StateHasChanged();
+                }
+            }
+        }
+    }
+}
