0e6e429e2394f2b7ab526b231e5713d1b227eccc
Build server <1bc13e627569438090d7133abce39de166eb0ff3>
2019-05-31 21:49:40 +0200

    Auto-update GitHub pages: '1.0.0-alpha0059'

--- _content/Blazorc.PatternFly/ElementsFunctionsInterop.js
+++ _content/Blazorc.PatternFly/ElementsFunctionsInterop.js
@@ -1,12 +1,3 @@
-// This file is to show how a library package may provide JavaScript interop features
-// wrapped in a .NET API
-
-window.exampleJsFunctions = {
-  showPrompt: function (message) {
-    return prompt(message, 'Type anything here');
-  }
-};
-
 // TODO: Move to another place and fix the name.
 window.ElementsFunctions = {
     getBoundingClientRect: function(x, y) {
