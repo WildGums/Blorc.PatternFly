31bb012e8c3953b701a7b19d75f12d373f14ea77
Geert van Horrik <geert@catenalogic.com>
2019-05-31 11:05:09 +0200

    Add TextArea component

---
+++ src/Blazorc.PatternFly/Components/TextArea/TextAreaComponent.cs
@@ -0,0 +1,25 @@
+﻿namespace Blazorc.PatternFly.Components.TextArea
+{
+    using System;
+    using Microsoft.AspNetCore.Components;
+
+    public class TextAreaComponent : ComponentBase
+    {
+        public TextAreaComponent()
+        {
+            IsValid = true;
+        }
+
+        [Parameter]
+        public string Label { get; set; }
+
+        [Parameter]
+        public bool IsRequired { get; set; }
+
+        [Parameter]
+        public bool IsValid { get; set; }
+
+        [Parameter]
+        public string Value { get; set; }
+    }
+}
