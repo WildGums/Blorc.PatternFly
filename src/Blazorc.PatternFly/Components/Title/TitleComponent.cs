d3014b41ab42bd25963956e451640d2439ed61ba
Geert van Horrik <geert@catenalogic.com>
2019-05-31 11:44:15 +0200

    Add Title component

---
+++ src/Blazorc.PatternFly/Components/Title/TitleComponent.cs
@@ -0,0 +1,23 @@
+﻿namespace Blazorc.PatternFly.Components.Title
+{
+    using System;
+    using Microsoft.AspNetCore.Components;
+
+    public class TitleComponent : ComponentBase
+    {
+        public TitleComponent()
+        {
+            Size = "xl";
+            HeadingLevel = "h1";
+        }
+
+        [Parameter]
+        public string Size { get; set; }
+
+        [Parameter]
+        public string HeadingLevel { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
