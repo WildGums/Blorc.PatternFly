889673147f34e6b4abd1cc17a2801b354dfa322f
Geert van Horrik <geert@catenalogic.com>
2019-05-31 21:45:50 +0200

    Add switch component

---
+++ src/Blazorc.PatternFly/Components/Switch/SwitchComponent.cs
@@ -0,0 +1,25 @@
+﻿namespace Blazorc.PatternFly.Components.Switch
+{
+    using System;
+    using Microsoft.AspNetCore.Components;
+
+    public class SwitchComponent : ComponentBase
+    {
+        public SwitchComponent()
+        {
+            IsChecked = true;
+        }
+
+        [Parameter]
+        public string Label { get; set; }
+
+        [Parameter]
+        public bool IsChecked { get; set; }
+
+        [Parameter]
+        public bool IsDisabled { get; set; }
+
+        [Parameter]
+        public Action OnChange { get; set; }
+    }
+}
