da4367f73330ff507b383eb373a6b5cf5546c27c
Geert van Horrik <geert@catenalogic.com>
2019-06-03 11:31:29 +0200

    Add input group

---
+++ src/Blazorc.PatternFly/Components/InputGroup/InputGroupTextComponent.cs
@@ -0,0 +1,25 @@
+﻿namespace Blazorc.PatternFly.Components.InputGroup
+{
+    using System;
+    using Microsoft.AspNetCore.Components;
+
+    public class InputGroupTextComponent : ComponentBase
+    {
+        public InputGroupTextComponent()
+        {
+            Component = "span";
+        }
+
+        [Parameter]
+        public string Component { get; set; }
+
+        [Parameter]
+        public string Label { get; set; }
+
+        [Parameter]
+        public string HtmlFor { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
