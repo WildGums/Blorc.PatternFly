4f4e518e8aca2a38ec0a47dedec497d65a53ae74
Geert van Horrik <geert@catenalogic.com>
2019-06-03 21:34:13 +0200

    Add Label component

---
+++ src/Blazorc.PatternFly/Components/Label/LabelComponent.cs
@@ -0,0 +1,34 @@
+﻿namespace Blazorc.PatternFly.Components.Label
+{
+    using System;
+    using Microsoft.AspNetCore.Components;
+
+    public class LabelComponent : ComponentBase
+    {
+        public LabelComponent()
+        {
+
+        }
+
+        public string Class
+        {
+            get
+            {
+                var value = string.Empty;
+
+                if (IsCompact)
+                {
+                    value += "pf-m-compact";
+                }
+
+                return value;
+            }
+        }
+
+        [Parameter]
+        public bool IsCompact { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
