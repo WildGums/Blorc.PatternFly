7a13c3a43e1ff56ce7cf281214de1da6d6598bf7
Geert van Horrik <geert@catenalogic.com>
2019-06-03 10:40:53 +0200

    Add FortAwesome icons

---
+++ src/Blazorc.PatternFly/Components/Icon/IconComponentBase.cs
@@ -0,0 +1,51 @@
+﻿namespace Blazorc.PatternFly.Components.Icon
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.AspNetCore.Components.RenderTree;
+
+    public abstract class IconComponentBase : ComponentBase
+    {
+        public IconComponentBase()
+        {
+            Fill = "currentColor";
+            Width = "1em";
+            Height = "1em";
+            Transform = string.Empty;
+        }
+
+        public abstract string Path { get; }
+
+        public abstract string ViewBox { get; }
+
+        public string Transform { get; set; }
+
+        public string Fill { get; set; }
+
+        public string Width { get; set; }
+
+        public string Height { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+
+            builder.OpenElement(1, "svg");
+
+            builder.AddAttribute(2, "fill", Fill);
+            builder.AddAttribute(3, "height", Height);
+            builder.AddAttribute(4, "width", Width);
+            builder.AddAttribute(5, "viewBox", ViewBox);
+            builder.AddAttribute(6, "aria-hidden", "true");
+            builder.AddAttribute(7, "role", "img");
+            builder.AddAttribute(8, "style", "vertical-align: -0.125em;");
+
+            builder.OpenElement(9, "path");
+            builder.AddAttribute(10, "d", Path);
+            builder.AddAttribute(11, "transform", Transform);
+            builder.CloseElement();
+            builder.CloseElement();
+        }
+    }
+}
---
+++ src/Blazorc.PatternFly/Components/Icon/src/readme.txt
@@ -0,0 +1,8 @@
+The source files come from:
+
+* https://github.com/FortAwesome/Font-Awesome (Font Awesome Free License)
+  * fortawesome-all.js => https://github.com/FortAwesome/Font-Awesome/blob/master/js/all.js
+  
+* https://github.com/patternfly/patternfly-react (MIT)
+  * pfreact-customIcons.js => https://github.com/patternfly/patternfly-react/tree/master/packages/react-icons/build/customIcons.js
+  * pfreact-pfIcons => https://github.com/patternfly/patternfly-react/tree/master/packages/react-icons/build/pfIcons.js
