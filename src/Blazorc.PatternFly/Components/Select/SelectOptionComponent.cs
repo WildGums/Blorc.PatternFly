23e32581baaa82a79390503b5a117c609aa37038
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2019-06-05 00:21:08 -0400

    Implementing single select input (work in progress)

--- src/Blazorc.PatternFly/Components/Select/SelectOptionComponent.cs
+++ src/Blazorc.PatternFly/Components/Select/SelectOptionComponent.cs
@@ -3,21 +3,28 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
+    using Icon;
     using Microsoft.AspNetCore.Components;
 
     public class SelectOptionComponent : ComponentBase
     {
         public SelectOptionComponent()
         {
-
         }
+
+        [Parameter]
+        public SelectComponent Parent { get; set; }
 
         public string Class
         {
             get
             {
                 var items = new List<string>();
-
+                if (IsSelected)
+                {
+                    items.Add("pf-m-selected");
+                }
 
                 return string.Join(" ", items);
             }
@@ -32,13 +39,38 @@
         [Parameter]
         public bool IsPlaceholder { get; set; }
 
+        public bool IsSelected
+        {
+            get
+            {
+                Console.WriteLine(Parent.SelectedItems.Count);
+                var item = Parent.SelectedItems.OfType<Tuple<string, string>>().FirstOrDefault(tuple => tuple.Item1 == Key);
+                return item != null;
+            }
+        }
+
         [Parameter]
-        public bool IsSelected { get; set; }
+        public string Key { get; set; }
+
+        [Parameter]
+        public string Value { get; set; }
 
         [Parameter]
         public RenderFragment ChildContent { get; set; }
 
         [Parameter]
         public EventHandler<EventArgs> Clicked { get; set; }
+
+        protected void OptionClicked()
+        {
+            if (!IsSelected)
+            {
+                Parent.SelectItem(Key, Value);
+            }
+            else
+            {
+                Parent.UnselectItem(Key);
+            }
+        }
     }
 }
