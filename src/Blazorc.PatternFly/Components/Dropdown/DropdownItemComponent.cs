d55e5569fab9c4f427321ce07067c1092cab5db7
Geert van Horrik <geert@catenalogic.com>
2019-06-06 14:19:26 +0200

    Implement DropdownItem and Separator

--- src/Blazorc.PatternFly/Components/Dropdown/DropdownItemComponent.cs
+++ src/Blazorc.PatternFly/Components/Dropdown/DropdownItemComponent.cs
@@ -1,7 +1,6 @@
 ﻿namespace Blazorc.PatternFly.Components.Dropdown
 {
     using System;
-    using System.Collections.Generic;
     using Microsoft.AspNetCore.Components;
 
     public class DropdownItemComponent : BlazorcComponentBase
@@ -10,27 +9,57 @@
         {
             Component = "a";
             Index = -1;
+
+            CreateConverter()
+                .Fixed("pf-c-dropdown__menu-item")
+                .If(() => IsDisabled, "pf-m-disabled")
+                .Watch(() => IsDisabled)
+                .Update(() => Class);
         }
 
         public string Class { get; set; }
 
         [Parameter]
-        public string Component { get; set; }
+        public string Component
+        {
+            get { return GetPropertyValue<string>(nameof(Component)); }
+            set { SetPropertyValue(nameof(Component), value); }
+        }
 
         [Parameter]
-        public string Key { get; set; }
+        public string Key
+        {
+            get { return GetPropertyValue<string>(nameof(Key)); }
+            set { SetPropertyValue(nameof(Key), value); }
+        }
 
         [Parameter]
-        public bool IsDisabled { get; set; }
+        public bool IsDisabled
+        {
+            get { return GetPropertyValue<bool>(nameof(IsDisabled)); }
+            set { SetPropertyValue(nameof(IsDisabled), value); }
+        }
 
         [Parameter]
-        public bool IsHovered { get; set; }
+        public bool IsHovered
+        {
+            get { return GetPropertyValue<bool>(nameof(IsHovered)); }
+            set { SetPropertyValue(nameof(IsHovered), value); }
+        }
 
         [Parameter]
-        public string Href { get; set; }
+        public string Href
+        {
+            get { return GetPropertyValue<string>(nameof(Href)); }
+            set { SetPropertyValue(nameof(Href), value); }
+        }
 
         [Parameter]
-        public int Index { get; set; }
+        public int Index
+        {
+            get { return GetPropertyValue<int>(nameof(Index)); }
+            set { SetPropertyValue(nameof(Index), value); }
+        }
 
         [Parameter]
         public RenderFragment ChildContent { get; set; }
--- src/Blazorc.PatternFly/Components/Dropdown/SeparatorComponent.cs
+++ src/Blazorc.PatternFly/Components/Dropdown/SeparatorComponent.cs
@@ -8,42 +8,51 @@
     {
         public SeparatorComponent()
         {
-            Component = "a";
+            Component = "div";
+
+            CreateConverter()
+                .Fixed("pf-c-dropdown__separator")
+                .If(() => IsDisabled, "pf-m-disabled")
+                .Watch(() => IsDisabled)
+                .Update(() => Class);
         }
 
-        public string Class
+        public string Class { get; set; }
+
+        [Parameter]
+        public string Key
         {
-            get
-            {
-                var items = new List<string>();
-
-                //if (IsRead)
-                //{
-                //    items.Add("pf-m-read");
-                //}
-                //else
-                //{
-                //    items.Add("pf-m-unread");
-                //}
-
-                return string.Join(" ", items);
-            }
+            get { return GetPropertyValue<string>(nameof(Key)); }
+            set { SetPropertyValue(nameof(Key), value); }
         }
 
         [Parameter]
-        public string Key { get; set; }
+        public string Component
+        {
+            get { return GetPropertyValue<string>(nameof(Component)); }
+            set { SetPropertyValue(nameof(Component), value); }
+        }
 
         [Parameter]
-        public string Component { get; set; }
+        public bool IsDisabled
+        {
+            get { return GetPropertyValue<bool>(nameof(IsDisabled)); }
+            set { SetPropertyValue(nameof(IsDisabled), value); }
+        }
 
         [Parameter]
-        public bool IsDisabled { get; set; }
+        public bool IsHovered
+        {
+            get { return GetPropertyValue<bool>(nameof(IsHovered)); }
+            set { SetPropertyValue(nameof(IsHovered), value); }
+        }
 
         [Parameter]
-        public bool IsHovered { get; set; }
-
-        [Parameter]
-        public string Href { get; set; }
+        public string Href
+        {
+            get { return GetPropertyValue<string>(nameof(Href)); }
+            set { SetPropertyValue(nameof(Href), value); }
+        }
 
         [Parameter]
         public RenderFragment ChildContent { get; set; }
