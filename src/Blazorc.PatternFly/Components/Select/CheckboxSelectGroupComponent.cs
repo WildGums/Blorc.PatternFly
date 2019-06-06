20343754f2a3c72ace040b8cd567e5e5b02028bc
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2019-06-06 14:11:16 -0400

    Implement group checkbox select

--- src/Blazorc.PatternFly/Components/Select/CheckboxSelectGroupComponent.cs
+++ src/Blazorc.PatternFly/Components/Select/CheckboxSelectGroupComponent.cs
@@ -1,30 +1,15 @@
 ﻿namespace Blazorc.PatternFly.Components.Select
 
 {
-    using System;
-    using System.Collections.Generic;
     using Microsoft.AspNetCore.Components;
 
     public class CheckboxSelectGroupComponent : BlazorcComponentBase
     {
-        public CheckboxSelectGroupComponent()
-        {
-
-        }
-
-        public string Class
-        {
-            get
-            {
-                var items = new List<string>();
-
-
-                return string.Join(" ", items);
-            }
-        }
-
         [Parameter]
         public string Label { get; set; }
+
+        [Parameter]
+        public string Key { get; set; }
 
         [Parameter]
         public RenderFragment ChildContent { get; set; }
