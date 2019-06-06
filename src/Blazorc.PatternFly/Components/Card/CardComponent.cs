94871f637ea641638c68b7b6fe0de2f598ed8655
Geert van Horrik <geert@catenalogic.com>
2019-06-06 16:45:48 +0200

    Add Card component

---
+++ src/Blazorc.PatternFly/Components/Card/CardComponent.cs
@@ -0,0 +1,29 @@
+﻿namespace Blazorc.PatternFly.Components.Card
+{
+    using Microsoft.AspNetCore.Components;
+
+    public class CardComponent : BlazorcComponentBase
+    {
+        public CardComponent()
+        {
+            Component = "article";
+
+            CreateConverter()
+                .Fixed("pf-c-card")
+                .If(() => IsHoverable, "pf-m-hoverable")
+                .Watch(() => IsHoverable)
+                .Update(() => Class);
+        }
+
+        public string Class { get; set; }
+
+        [Parameter]
+        public string Component { get; set; }
+
+        [Parameter]
+        public bool IsHoverable { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
