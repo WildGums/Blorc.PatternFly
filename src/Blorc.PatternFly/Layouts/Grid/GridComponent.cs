1612bd86995546bd9ff78d00e81e2070e96d23bf
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2019-10-19 19:39:23 -0400

    Implement Grid Layout

---
+++ src/Blorc.PatternFly/Layouts/Grid/GridComponent.cs
@@ -0,0 +1,30 @@
+﻿namespace Blorc.PatternFly.Layouts.Grid
+{
+    using Blorc.Components;
+    using Microsoft.AspNetCore.Components;
+    using StateConverters;
+
+    public class GridComponent : BlorcComponentBase
+    {
+        public GridComponent()
+        {
+            CreateConverter()
+                .Fixed("pf-l-grid")
+                .Watch(() => IsGutter)
+                .If(() => IsGutter, "pf-m-gutter")
+                .Update(() => Class);
+        }
+
+        public string Class { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public bool IsGutter
+        {
+            get => GetPropertyValue<bool>(nameof(IsGutter));
+            set => SetPropertyValue(nameof(IsGutter), value);
+        }
+    }
+}
---
+++ src/Blorc.PatternFly/Layouts/Grid/GridItemComponent.cs
@@ -0,0 +1,39 @@
+﻿namespace Blorc.PatternFly.Layouts.Grid
+{
+    using Blorc.Components;
+    using Microsoft.AspNetCore.Components;
+    using StateConverters;
+
+    public class GridItemComponent : BlorcComponentBase
+    {
+        public GridItemComponent()
+        {
+            CreateConverter()
+                .Fixed("pf-l-grid__item")
+                .Watch(() => ColSpan)
+                .Watch(() => RowSpan)
+                .If(() => ColSpan >= 2, () => $"pf-m-{ColSpan}-col")
+                .If(() => RowSpan >= 2, () => $"pf-m-{RowSpan}-row")
+                .Update(() => Class);
+        }
+
+        public string Class { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public int ColSpan
+        {
+            get { return GetPropertyValue<int>(nameof(ColSpan)); }
+            set { SetPropertyValue(nameof(ColSpan), value); }
+        }
+
+        [Parameter]
+        public int RowSpan
+        {
+            get { return GetPropertyValue<int>(nameof(RowSpan)); }
+            set { SetPropertyValue(nameof(RowSpan), value); }
+        }
+    }
+}
