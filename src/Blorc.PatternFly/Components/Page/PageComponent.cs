bc2629cdac9e5016f084926f2e454c7df9f87f19
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2019-12-13 12:26:23 -0500

    Implement Page component

---
+++ src/Blorc.PatternFly/Components/Page/PageComponent.cs
@@ -0,0 +1,37 @@
+﻿namespace Blorc.PatternFly.Components.Page
+{
+    using Blorc.Components;
+
+    using Microsoft.AspNetCore.Components;
+
+    public class PageComponent : BlorcComponentBase
+    {
+        [Parameter]
+        public RenderFragment LogoContent { get; set; }
+
+        [Parameter]
+        public RenderFragment MainContent { get; set; }
+
+        public SidebarComponent Sidebar { get; set; }
+
+        [Parameter]
+        public RenderFragment SidebarContent { get; set; }
+
+        public string Theme
+        {
+            get => GetPropertyValue<string>(nameof(Theme));
+            set => SetPropertyValue(nameof(Theme), value);
+        }
+
+        [Parameter]
+        public RenderFragment ToolbarContent { get; set; }
+
+        protected void OnClickNavbarToogle()
+        {
+            if (Sidebar != null)
+            {
+                Sidebar.IsExpanded = !Sidebar.IsExpanded;
+            }
+        }
+    }
+}
---
+++ src/Blorc.PatternFly/Components/Page/SidebarComponent.cs
@@ -0,0 +1,62 @@
+﻿namespace Blorc.PatternFly.Components.Page
+{
+    using Blorc.Components;
+    using Blorc.StateConverters;
+
+    using Microsoft.AspNetCore.Components;
+
+    public class SidebarComponent : BlorcComponentBase
+    {
+        public SidebarComponent()
+        {
+            CreateConverter()
+                .Fixed("pf-c-page__sidebar")
+                .If(() => IsExpanded, "pf-m-expanded")
+                .Update(() => Class);
+        }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        public string Class
+        {
+            get => GetPropertyValue<string>(nameof(Class));
+            set => SetPropertyValue(nameof(Class), value);
+        }
+
+        [Parameter]
+        public bool IsExpanded
+        {
+            get => GetPropertyValue<bool>(nameof(IsExpanded));
+            set => SetPropertyValue(nameof(IsExpanded), value);
+        }
+
+        [CascadingParameter]
+        public PageComponent PageComponent { get; set; }
+
+        [Parameter]
+        public string Theme
+        {
+            get => GetPropertyValue<string>(nameof(Theme));
+            set => SetPropertyValue(nameof(Theme), value);
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            if (PageComponent != null)
+            {
+                Theme = PageComponent.Theme;
+            }
+        }
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            if (PageComponent != null)
+            {
+                StateHasChanged();
+            }
+        }
+    }
+}
