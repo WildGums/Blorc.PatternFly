3971d6707ba8bf82e73898e119142871500b059f
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2019-12-13 20:01:44 -0500

    Improve Example App

---
+++ src/Blorc.PatternFly.Example/Components/NavigationItemContent/NavigationItemContentComponent.cs
@@ -0,0 +1,36 @@
+﻿namespace Blorc.PatternFly.Example.Components.NavigationItemContent
+{
+    using System.ComponentModel;
+
+    using Blorc.Components;
+
+    using Microsoft.AspNetCore.Components;
+
+    public class NavigationItemContentComponent : BlorcComponentBase
+    {
+        [Parameter]
+        public string Text
+        {
+            get => GetPropertyValue<string>(nameof(Text));
+            set => SetPropertyValue(nameof(Text), value);
+        }
+        
+        [Parameter]
+        public ItemType ItemType
+        {
+            get => GetPropertyValue<ItemType>(nameof(ItemType));
+            set => SetPropertyValue(nameof(ItemType), value);
+        }
+    }
+
+    public enum ItemType
+    {
+        Unchanged,
+
+        New,
+        
+        Updated,
+
+        Unavailable
+    }
+}
