b2bd9d83cb41ae4f6d8e27400404ce395ca1377f
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2020-01-11 01:03:24 -0500

    Update Select examples

--- src/Blorc.PatternFly.Example/Pages/Components/SelectDemoComponent.cs
+++ src/Blorc.PatternFly.Example/Pages/Components/SelectDemoComponent.cs
@@ -1,6 +1,7 @@
 ﻿namespace Blorc.PatternFly.Example.Pages.Components
 {
     using System;
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.ComponentModel;
     using Blorc.Components;
@@ -11,7 +12,18 @@
         {
             base.OnInitialized();
             SelectedItems2 = new ObservableCollection<string> {"1"};
+            SelectedItems4 = new ObservableCollection<string> { "1" };
+            DataSource = new List<Tuple<string, string>>()
+            {
+                new Tuple<string, string>("0", "Active"),
+                new Tuple<string, string>("1", "Cancelled"),
+                new Tuple<string, string>("2", "Paused"),
+                new Tuple<string, string>("3", "Warning"),
+                new Tuple<string, string>("4", "Restarted"),
+            };
         }
+
+        public List<Tuple<string, string>> DataSource { get; set; }
 
         public ObservableCollection<string> SelectedItems
         {
@@ -25,23 +37,49 @@
             set => SetPropertyValue(nameof(SelectedItems2), value);
         }
 
+        public ObservableCollection<string> SelectedItems3
+        {
+            get => GetPropertyValue<ObservableCollection<string>>(nameof(SelectedItems3));
+            set => SetPropertyValue(nameof(SelectedItems3), value);
+        }
+
+        public ObservableCollection<string> SelectedItems4
+        {
+            get => GetPropertyValue<ObservableCollection<string>>(nameof(SelectedItems4));
+            set => SetPropertyValue(nameof(SelectedItems4), value);
+        }
+
         protected override void OnPropertyChanged(PropertyChangedEventArgs e)
         {
             base.OnPropertyChanged(e);
             if (e.PropertyName == nameof(SelectedItems) && SelectedItems != null)
             {
+                Console.WriteLine($"Changed SelectedItems: {SelectedItems2.Count}");
                 SelectedItems.CollectionChanged += (sender, args) =>
                 {
                     Console.WriteLine($"SelectedItems: {SelectedItems.Count}");
                 };
             }
-
-            if (e.PropertyName == nameof(SelectedItems2) && SelectedItems2 != null)
+            else if (e.PropertyName == nameof(SelectedItems2) && SelectedItems2 != null)
             {
                 Console.WriteLine($"Changed SelectedItems2: {SelectedItems2.Count}");
                 SelectedItems2.CollectionChanged += (sender, args) =>
                 {
                     Console.WriteLine($"SelectedItems2: {SelectedItems2.Count}");
+                };
+            } else if (e.PropertyName == nameof(SelectedItems3) && SelectedItems3 != null)
+            {
+                Console.WriteLine($"Changed SelectedItems3: {SelectedItems3.Count}");
+                SelectedItems3.CollectionChanged += (sender, args) =>
+                {
+                    Console.WriteLine($"SelectedItems3: {SelectedItems3.Count}");
+                };
+            } else if (e.PropertyName == nameof(SelectedItems4) && SelectedItems4 != null)
+            {
+                Console.WriteLine($"Changed SelectedItems4: {SelectedItems4.Count}");
+                SelectedItems4.CollectionChanged += (sender, args) =>
+                {
+                    Console.WriteLine($"SelectedItems4: {SelectedItems4.Count}");
                 };
             }
         }
