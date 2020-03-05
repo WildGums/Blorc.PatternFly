adb4baa2f29f7b30ab15dc214afef27a657d160a
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2020-03-05 00:48:52 -0500

    REmove PatternFlyLayoutComponentBase

---
+++ src/Blorc.PatternFly/Services/Extensions/DocumentServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Blorc.PatternFly.Services.Extensions
+{
+    using System.Threading.Tasks;
+
+    using Blorc.Services;
+
+    public static class DocumentServiceExtensions
+    {
+        public static async Task InjectBlorcPatternFly(this IDocumentService documentService)
+        {
+            await documentService.InjectAssemblyCSSFile(typeof(DocumentServiceExtensions).Assembly, "patternfly/patternfly.css");
+        }
+    }
+}
