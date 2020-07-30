89b7a70a771bdb32e3f1d1ca8819a1962b3f2d73
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2020-07-30 00:14:58 -0400

    Update package references

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Reflection;
 [assembly: AssemblyCompany("WildGums")]
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.430")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.560")]
 [assembly: AssemblyCopyright("Copyright © WildGums 2019 - 2020")]
 
